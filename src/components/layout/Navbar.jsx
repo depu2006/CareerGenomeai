8b865ca7c1cb08472cb31c5a49b7b76b90081ec3
Shree <deepshika@gmail.com>
2026-02-16 23:06:11 +0530

    Fix AnimatePresence import

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeContext';
 import { useAuth } from '../../context/AuthContext';
